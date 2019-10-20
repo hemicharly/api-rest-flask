8ccd12edd03870fc0ae3b2511803306db57410f0
hemicharly <hemicharlythiago@gmail.com>
2019-10-20 10:24:00 -0300

    feat: Changed userDto

--- app/main/models/dto/UserDto.py
+++ app/main/models/dto/UserDto.py
@@ -3,8 +3,8 @@ from flask_restplus import Namespace, fields
 class UserDto:
     api = Namespace('User', description='User related operations')
     user = api.model('user', {
+        'id': fields.String(description='user Identifier'),
         'email': fields.String(required=True, description='user email address'),
         'username': fields.String(required=True, description='user username'),
-        'password': fields.String(required=True, description='user password'),
-        'public_id': fields.String(description='user Identifier')
+        'password': fields.String(required=True, description='user password')
     })
